a149180d32a4faea0532942992f751c70b9b3b09
Mehtab Chithiwala <mehtab94@gmail.com>
2015-03-08 04:24:22 -0400

    Changed Portfolio

--- index.js
+++ index.js
@@ -1,137 +1,108 @@
 $(function() {
-    $("#box-5").backstretch("Images/currency.jpg");
-    $("#box-2").backstretch("Images/maze.gif");
-    $("#box-3").backstretch("Images/fingers.jpg");
-    $("#box-4").backstretch("Images/robin.jpg");
-    $("#landing-page").backstretch("Images/fingers.jpg");
-    //$("#pic").backstretch("Images/Me.jpg");
-});
 
-var h = $(window).height();
-
-
-var k = "Hi, I'm Mehtab Chithiwala. I'm currently attending SPSU for Computer Science, and hope to graduate in Spring 2015. I'm also interning at Georgia Pacific as a Software Developer. In my free time, I like to dabble in new technologies and create interesting projects, some of which can be seen in my portfolio below. I am also a huge sports fan (Go Falcons!) and like to volunteer around the community in my spare time. If you would like to get in touch with me, feel free to contact me via LinkedIn, Email, or even Facebook.";
-
-$(function() {
-    $(window).scroll(function() {
-        var y_scroll_pos = window.pageYOffset;
-        var scroll_pos_test = h-(h/9);             
-
-        if(y_scroll_pos > scroll_pos_test) {
-           $("#typer-box").typer(
-            [k]);
-            $(window).unbind('scroll');
-        }
-    });
-});
-
-
-$(document).ready(function(){
-	$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-
-	    var target = this.hash;
-	    var $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing');
-	});   
-});
-
-$(function() {
-    $(".project-full").hide();
-    $(".project-full").click(function() {
-        $(".project-full").hide();
-    });
-});
-
-$(function() {
-    $("#box-5").click(
-        function() {
-            $("#box-5").hide();
-            $("#cont-5").show();
-    });
-    $("#box-2").click(
-        function() { 
-            $("#box-2").hide();
-            $("#cont-2").show();
-    });
-    $("#box-3").click(
-        function() { 
-            $("#box-3").hide();
-            $("#cont-3").show();
-    });
-    $("#box-4").click(
-        function() { 
-            $("#box-4").hide();
-            $("#cont-4").show();
-    });
-    $("#cont-5").click(
-        function() { 
-            $("#cont-5").hide();
-            $("#box-5").show();
-    });
-    $("#cont-2").click(
-        function() { 
-            $("#cont-2").hide();
-            $("#box-2").show();
-    });
-    $("#cont-3").click(
-        function() { 
-            $("#cont-3").hide();
-            $("#box-3").show();
-    });
-    $("#cont-4").click(
-        function() { 
-            $("#cont-4").hide();
-            $("#box-4").show();
-    });
-});
-
-var w = $(window).width();
-
-$(function() {
-    $(".cont").hide();
-    $(".name-box").hide();
-    $(".header-2").hide();
-    $(".header-icon").click(function() {
-        $(".header-2").toggle();
+    
+    $(".content").hide();
+    $(".landing").show();
+    $(".description").hide();
+    
+    $("#about-button").click(function() {
+        $(".content").hide();
+        $(".about").show();
     });
     
-    if (w > 800)
-    {
-        $(".header-icon").hide();
+    $("#portfolio-button").click(function() {
+        $(".content").hide();
+        $(".portfolio").show();
+    });
+    
+    $("#contact-button").click(function() {
+        $(".content").hide();
+        $(".contact").show();
+    });
+    
+  /*  var ikraContent = "Ikra allows users to read verses of the Quran by a specified Ayat, a complete Sura, or by Keywords. I initially made this because a few friends and I got tired of skimming through the Quran to find a specific topic and wanted a keyword search.";
+    
+    var currencyContent = "A Currency Converter that gets the latest currency data for over 165 countries and allows free conversion between them. The same application was also ported as a Java App, using the GSON library to parse the JSON from the API calls.";
+    
+    var socketsContent = "Sockets is a turn based game made in Java to compete with fellow classmates. Each player has a rat and a maze, and the rat tries to solve other people's mazes in the most optimal way. The rats communicated with the maze over the server using TCP/IP Socket communication, hence the name.";
+    
+    var schedulerContent = "A scheduling system written in Java, which implements the Round-Robin Scheduling Algorithm. The system involved all aspects of the system, including the entry of teams, scheduling, scoring, time management, and UI.";
+    
+    */
+    
+    $(".project").click(function() {
+        $(".portfolio").hide();
+    });
+    
+    $("#ikra").click(function() {
+        $("#ikra-des").show();
+    });
+    
+    $("#currency").click(function() {
+        $("#currency-des").show();
+    });
+    
+    $("#sockets").click(function() {
+        $("#sockets-des").show();
+    });
+    
+    $("#scheduler").click(function() {
+        $("#scheduler-des").show();
+    });
+    
+    
+    $(".but").click(function() {
+        $(".description").hide();
+        $(".portfolio").show();
+    });
+    
+    
+    /*
+    
+    $("#ikra").hover(function() {
+        $("#ikra-des").show();
+    }, function() {
+        $("#ikra-des").hide();
+    });   
+    
+     $("#currency").hover(function() {
+        $("#currency-des").show();
+    }, function() {
+        $("#currency-des").hide();
+    });
+    
+     $("#sockets").hover(function() {
+        $("#sockets-des").show();
+    }, function() {
+        $("#sockets-des").hide();
+    });
+    
+     $("#scheduler").hover(function() {
+        $("#scheduler-des").show();
+    }, function() {
+        $("#scheduler-des").hide();
+    });
+
+    
+    */
+    
+    var h = $(window).height();
+
+
+    var about = "Hi, I'm Mehtab Chithiwala. I'm currently attending SPSU for Computer Science, and hope to graduate in Spring 2015. I'm also interning at Georgia Pacific as a Software Developer. In my free time, I like to dabble in new technologies and create interesting projects, some of which can be seen in my portfolio below. I am also a huge sports fan (Go Falcons!) and like to volunteer around the community in my spare time. If you would like to get in touch with me, feel free to contact me via LinkedIn, Email, or even Facebook.";
+    
+    var intro = "Software Developer, Atlanta.";
+    var intro2 = "Currently studying at SPSU and Interning at Georgia Pacfic.";
+
+    
+    var typeDetail = function() {
+        $(".lp").typer([intro2]);
     }
     
-    if (w < 801)
-    {
-        $(".header").hide();
+    var typeIntro = function() {
+        $(".lh").typer([intro]);
     }
-});
-
-
-$(window).resize(function() {
-    var wd = $(window).width();
-    if (wd < 801)
-    {
-        $(".header").hide();
-        $(".header-icon").show();
-    }
-    else
-    {
-        $(".header").show();
-        $(".header-icon").hide();
-        $(".header-2").hide();
-    }
-});
-
-
-$(function() {
-    $("a").click(function() {
-        if (($(".header-2").css("display") != "none"))
-        {
-            $(".header-2").hide();
-        }
-    });
+    
+    typeIntro();
+    setTimeout(typeDetail, 2200);
 });
